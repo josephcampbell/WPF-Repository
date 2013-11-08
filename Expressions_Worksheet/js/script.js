2fc2ad814b2972698bc9923c2a47b42536b633ca
Joseph Campbell <josephcampbell@fullsail.edu>
2013-11-08 15:57:30 -0600

    Dog Years problem complete

---
+++ Expressions_Worksheet/js/script.js
@@ -0,0 +1,12 @@
+// Joseph Campbell 11-07-2013 Expression Worksheet
+
+// Problem: Calculate sparky's age in dog years given it's actual age.
+
+// Sparky's age.
+var sparkyAge = 3
+
+// Sparky's age in dog years.
+var result = sparkyAge * 7
+
+// Result to print.
+console.log("Sparky is" + " " + sparkyAge + " " + "human years old which is" + " " + result + " " + "in dog years.");
