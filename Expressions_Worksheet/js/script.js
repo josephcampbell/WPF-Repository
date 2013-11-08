1ea9010038cde4fc23e1cf7b98b76d915af7ba34
Joseph Campbell <josephcampbell@fullsail.edu>
2013-11-08 16:14:49 -0600

    Slice of Pie part ! complete

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -10,3 +10,22 @@ var result = sparkyAge * 7
 
 // Result to print.
 console.log("Sparky is" + " " + sparkyAge + " " + "human years old which is" + " " + result + " " + "in dog years.");
+
+// Problem: How much pizza will each parygoer get?
+
+// Slices per pizza.
+var perPizza = 8
+
+// Number of people at the party.
+var people = 12
+
+// Number of pizzas ordered.
+var numPizza = 3
+
+// Number of slices per person.
+var perPerson = numPizza*perPizza/people
+
+// Result to print.
+console.log("Each person ate" + ' ' + perPerson + ' ' + "slices of pizza at the party.")
+
+
