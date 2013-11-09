ffb1dedb6517ab168c8196cc67cab11a91982888
Joseph Campbell <josephcampbell@fullsail.edu>
2013-11-08 19:32:49 -0600

    Entered given information

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,1 +1,7 @@
 // Joseph Campbell 11-08-2013 Expressions_Personal
+
+// Problem: Calculate how much it would cost in fuel traveling by car.
+
+var distance = prompt("Please enter the distance you need to travel."); // This asks the user to enter how far they are planning to travel.
+var price = prompt("Please enter the price of the fuel you will be using."); // This asks the user to enter the price of fuel.
+var mpg = prompt("Please enter your vehicles miles per gallon.") // This asks the user to enter the mpg of their vehicle.
