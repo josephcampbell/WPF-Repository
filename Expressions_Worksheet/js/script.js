d57bf81799b0186f45b128be3ef8c939f76fb629
Joseph Campbell <josephcampbell@fullsail.edu>
2013-11-08 18:55:00 -0600

    Expressions worksheet complete (updated)
    shortened results by adding spaces

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -3,68 +3,68 @@
 // Problem: Calculate sparky's age in dog years given it's actual age.
 
 // Sparky's age.
-var sparkyAge = 3
+var sparkyAge = 3;
 // Sparky's age in dog years.
-var result = sparkyAge * 7
+var result = sparkyAge * 7;
 
 // Result to print.
-console.log("Sparky is" + " " + sparkyAge + " " + "human years old which is" + " " + result + " " + "in dog years.");
+console.log("Sparky is " + sparkyAge + " human years old which is " + result + " in dog years.");
 
 // Problem: How much pizza will each partygoer get?
 
 // Slices per pizza.
-var perPizza = 8
+var perPizza = 8;
 // Number of people at the party.
-var people = 12
+var people = 12;
 // Number of pizzas ordered.
-var numPizza = 3
+var numPizza = 3;
 // Number of slices per person.
-var perPerson = numPizza*perPizza/people
+var perPerson = numPizza*perPizza/people;
 
 // Result to print.
-console.log("Each person ate" + " " + perPerson + " " + "slices of pizza at the party.")
+console.log("Each person ate " + perPerson + " slices of pizza at the party.");
 
 // Problem: How many slices will Sparky get?
 
 // Total number of slices.
-var totalSlices = numPizza*perPizza
+var totalSlices = numPizza*perPizza;
 // Total number of slices eaten by partygoers.
-var eaten = people*perPerson
+var eaten = people*perPerson;
 // Number of slices left for Sparky.
-var forSparky = totalSlices-eaten
+var forSparky = totalSlices-eaten;
 
 // Result to print.
 
-console.log("Sparky got" + " " + forSparky + " " + "slices of pizza.")
+console.log("Sparky got " + forSparky + " slices of pizza.");
 
 // Problem: Total and average spent on groceries for the past five weeks.
 
 // Total spent each week.
-var weeklyTotals = [214, 107, 194, 228, 139]
+var weeklyTotals = [214, 107, 194, 228, 139];
 // Total amount spent on groceries.
-var total = weeklyTotals[0] + weeklyTotals[1] + weeklyTotals[2] + weeklyTotals[3] + weeklyTotals[4]
+var total = weeklyTotals[0] + weeklyTotals[1] + weeklyTotals[2] + weeklyTotals[3] + weeklyTotals[4];
 // Average weekly grocery spending.
-var average = total/5
+var average = total/5;
 
 // Result to print.
 
-console.log("You have spent a total of" + " " + "$" + total + " " + "on groceries over 5 weeks. That is an average of" + " " + "$" + average + " " + "per week.")
+console.log("You have spent a total of " + "$" + total + " on groceries over 5 weeks. That is an average of " + "$" + average + " per week.");
 
 // Problem: Calculate the discounted price for an item.
 
 // Original price of item.
-var original = 399
+var original = 399;
 // Discount percentage.
-var discount = .10
+var discount = .10;
 // Description of item.
-var itemDesc = "ipad"
+var itemDesc = "ipad";
 // Sales tax percentage.
-var tax = .06
+var tax = .06;
 // Price of the item without tax.
-var noTax = original-(original*discount)
+var noTax = original-(original*discount);
 // Price of the item with tax.
-var withTax = noTax+(noTax*tax)
+var withTax = noTax+(noTax*tax);
 
 
 // Result to print.
-console.log("Your" + " " + itemDesc + " " + "was originally" + " " + "$" + original + "," + " " + "but after a" + " " + discount + "%" + " " + "discount, it is now" + " " + "$" + noTax + " " + "without tax, and" + " " + "$" + withTax + " " + "with tax.") 
+console.log("Your " + itemDesc + " was originally " + "$" + original + ", " + "but after a " + discount + "%" + " discount, it is now " + "$" + noTax + " without tax, and " + "$" + withTax + " with tax.") ;
